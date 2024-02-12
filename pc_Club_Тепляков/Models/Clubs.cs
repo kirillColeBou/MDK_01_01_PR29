a5de8bea823998d515ff7a6a58a9682cc4b570ad
subbotin.59 <gsubbotin.2019@gmail.com>
2024-02-12 16:01:17 +0500

    Разработанные модели. Тепляков

---
+++ pc_Club_Тепляков/Models/Clubs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pc_Club_Тепляков.Models
+{
+    public class Clubs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string WorkTime { get; set; }
+    }
+}
---
+++ pc_Club_Тепляков/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pc_Club_Тепляков.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public DateTime RentStart { get; set; }
+        public int Duration { get; set; }
+        public int IdClub { get; set; }
+    }
+}
